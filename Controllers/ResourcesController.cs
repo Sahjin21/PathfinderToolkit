2a0fd6a5690d7540bd6856add025c88fbd95b4af
Shawn Embry <shawnembry15@gmail.com>
2023-03-10 15:24:37 -0500

    Ability's fully working, just need to added additional entries from json data for the table.

--- Controllers/ResourcesController.cs
+++ Controllers/ResourcesController.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using PathfinderToolkit.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
 using static PathfinderToolkit.Models.Resources;
+using Microsoft.AspNetCore.Http;
 
 namespace PathfinderToolkit.Controllers
 {
@@ -17,9 +21,31 @@ namespace PathfinderToolkit.Controllers
 
             ViewBag.Abilities = abilityList.Select(a => new SelectListItem { Text = a.name, Value = a.name }).ToList();
 
-            return View("~/Views/Home/Resources/Ability.cshtml", abilityList);
+            Resources.Ability selectedAbility = null;
+            if (Request.Method == "POST")
+            {
+                var selectedAbilityName = Request.Form["abilityDropdown"];
+                selectedAbility = abilityList.FirstOrDefault(a => a.name == selectedAbilityName);
+            }
+
+            return View("~/Views/Home/Resources/Ability.cshtml", selectedAbility);
         }
 
+        [HttpPost]
+        public IActionResult Ability(string abilityDropdown)
+        {
+            string jsonFilePath = "wwwroot/Data/Json PF/abilities.json";
+            string jsonString = System.IO.File.ReadAllText(jsonFilePath);
+
+            var json = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, List<Resources.Ability>>>(jsonString);
+            var abilityList = json["ability"].ToArray();
+
+            ViewBag.Abilities = abilityList.Select(a => new SelectListItem { Text = a.name, Value = a.name }).ToList();
+
+            var selectedAbility = abilityList.FirstOrDefault(a => a.name == abilityDropdown);
+
+            return View("~/Views/Home/Resources/Ability.cshtml", selectedAbility);
+        }
         public IActionResult Index()
         {
             /*string jsonFilePath = "wwwroot/Data/Json PF/test.json";
