60d076d4863d5e008f03015b157849152f422be9
Shawn Embry <shawnembry15@gmail.com>
2023-04-18 15:37:34 -0400

    Started code for login controller. Still in works.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,6 +11,7 @@ using Google.Apis.Util.Store;
 using System.Data.SqlClient;
 using System.Data;
 using Microsoft.Extensions.Configuration;
+using static PathfinderToolkit.Models.Resources;
 
 namespace PathfinderToolkit.Controllers
 {
@@ -30,38 +31,102 @@ namespace PathfinderToolkit.Controllers
 
         public IActionResult Login()
         {
-            string connString = _configuration.GetConnectionString("CONNECTIONSTRING");
-            try
-            {
-                // Table would be created ahead of time in production
-                using var conn = new SqlConnection(connString);
-                conn.Open();
-
-                /*var command = new SqlCommand(
-                    "CREATE TABLE Persons (ID int NOT NULL PRIMARY KEY IDENTITY, FirstName varchar(255), LastName varchar(255));",
-                    conn);*/
-                //using SqlDataReader reader = command.ExecuteReader();
-            }
-            catch (Exception e)
-            {
-                // Table may already exist
-                Console.WriteLine(e.Message);
-
-            }
             return View();
         }
 
-        [HttpPost]
-        public IActionResult Login(User user1)
+/*        [HttpPost]
+        public IActionResult LoginOrRegister(User user)
         {
-            User User = new User();
-            User.Username = user1.Username;
-            User.Password = user1.Password;
+            var db = new UserDatabaseOperations(_configuration);
 
-            // Redirect to the home page or another appropriate page
-            return RedirectToAction("Index");
+            // Check if the user exists in the database
+            var userFromDb = db.GetUserByUsernameAndPassword(user.Username, user.Password);
+
+            if (userFromDb != null)
+            {
+                // Redirect to the home page or another appropriate page
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // Check if the username already exists in the database
+                var existingUser = db.GetUserByUsername(user.Username);
+                if (existingUser != null)
+                {
+                    ModelState.AddModelError("", "Username already exists");
+                    return View();
+                }
+
+                // Create a new user and save it to the database
+                var newUser = new User
+                {
+                    Username = user.Username,
+                    IsAdmin = false,
+                    Password = user.Password
+                };
+                db.CreateUser(user.Username, user.Password, newUser.IsAdmin);
+
+                // Get the newly created user from the database
+                var createdUser = db.GetUserByUsername(newUser.Username);
+
+                // Log in the user and redirect to the home page or another appropriate page
+                return RedirectToAction("Index");
+            }
+        }*/
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            var db = new UserDatabaseOperations(_configuration);
+            var userFromDb = db.GetUserByUsernameAndPassword(user.Username, user.Password);
+            
+            if (userFromDb != null)
+            {
+                // Redirect to the home page or another appropriate page
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // Show an error message to the user
+                ModelState.AddModelError("", "Invalid username or password");
+                return View();
+            }
         }
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            var db = new UserDatabaseOperations(_configuration);
 
+            // Check if the username already exists in the database
+            var existingUser = db.GetUserByUsername(user.Username);
+            if (existingUser != null)
+            {
+                ModelState.AddModelError("", "Username already exists");
+                return View();
+            }
+
+            // Create a new user and save it to the database
+            var newUser = new User
+            {
+                Username = user.Username,
+                IsAdmin = false,
+                Password = user.Password            
+            };
+            db.CreateUser(user.Username, user.Password, newUser.IsAdmin);
+
+            // Get the newly created user from the database
+            var createdUser = db.GetUserByUsername(newUser.Username);
+
+            // Create a new instance of the User class and set its properties
+            var model = new User();
+            model.Username=createdUser.Username;
+            model.IsAdmin = false;
+            model.Password=createdUser.Password;
+
+            // Log in the user and redirect to the home page or another appropriate page
+            //HttpContext.Session.SetString("Username", createdUser.Username);
+            return RedirectToAction("Login", model);
+        }
         public IActionResult GM()
         {
             return View();
---
+++ Models/UserDatabaseOperations.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PathfinderToolkit.Models
+{
+    public class UserDatabaseOperations : User
+    {
+        private readonly IConfiguration _configuration;
+        private string connString;
+
+        public UserDatabaseOperations(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            connString = _configuration.GetConnectionString("CONNECTIONSTRING");
+        }
+
+        public User GetUserByUsernameAndPassword(string username, string password)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Users WHERE user=@Username AND password=@Password", conn);
+                cmd.Parameters.AddWithValue("@Username", username);
+                cmd.Parameters.AddWithValue("@Password", password);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        User userFromDb = new User();
+                        userFromDb.Id = Convert.ToInt32(reader["Id"]);
+                        userFromDb.Username = Convert.ToString(reader["Username"]);
+                        userFromDb.Password = Convert.ToString(reader["Password"]);
+                        userFromDb.IsAdmin = Convert.ToBoolean(reader["Admin"]);
+                        return userFromDb;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+        }
+
+        // Create a new user
+        public void CreateUser(string username, string password, bool admin)
+        {
+            using (var conn = new SqlConnection(connString))
+            {
+                var sql = @"INSERT INTO Users (user, password, admin) VALUES (@user, @password, @admin)";
+                using (var cmd = new SqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@user", username);
+                    cmd.Parameters.AddWithValue("@password", password);
+                    cmd.Parameters.AddWithValue("@admin", admin);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Update an existing user
+        public void UpdateUser(int id, string username, string password, bool admin)
+        {
+            using (var conn = new SqlConnection(connString))
+            {
+                var sql = @"UPDATE Users SET user = @user, password = @password, admin = @admin WHERE id = @id";
+                using (var cmd = new SqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.Parameters.AddWithValue("@user", username);
+                    cmd.Parameters.AddWithValue("@password", password);
+                    cmd.Parameters.AddWithValue("@admin", admin);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Get a user by username
+        public User GetUserByUsername(string name)
+        {
+            using (var conn = new SqlConnection(connString))
+            {
+                var sql = @"SELECT id, user, password, admin FROM Users WHERE username = @Username";
+                using (var cmd = new SqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Username", name);
+                    conn.Open();
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            var user = new User();
+                            user.Id = reader.GetInt32(0);
+                            user.Username = reader.GetString(1);
+                            user.Password = reader.GetString(2);
+                            user.IsAdmin = reader.GetBoolean(3);
+                            return user;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        // Get a user by ID
+        public User GetUserById(int id)
+        {
+            using (var conn = new SqlConnection(connString))
+            {
+                var sql = @"SELECT id, user, password, admin FROM Users WHERE id = @id";
+                using (var cmd = new SqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    conn.Open();
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            var user = new User();
+                            user.Id = reader.GetInt32(0);
+                            user.Username = reader.GetString(1);
+                            user.Password = reader.GetString(2);
+                            user.IsAdmin = reader.GetBoolean(3);
+                            return user;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        // Delete a user by ID
+        public void DeleteUserById(int id)
+        {
+            using (var conn = new SqlConnection(connString))
+            {
+                var sql = @"DELETE FROM Users WHERE id = @id";
+                using (var cmd = new SqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
