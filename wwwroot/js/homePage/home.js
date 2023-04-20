86948ecfc957474866e3d72bf8d1b3ed350d3a8f
HRM0 <103372533+HRM0@users.noreply.github.com>
2023-04-20 18:42:58 -0400

    changed how the li elements are generated for the announcements

--- wwwroot/js/homePage/home.js
+++ wwwroot/js/homePage/home.js
@@ -3,9 +3,7 @@ let announcementList = document.querySelector(".announceList")
 let announcements = ["this", "that", "evenmore of this and that", "Now working: Bestiary, Ability, Actions, Afflictions, and Dice Roller"]
 
 announcements.map(announcement => {
-    let listItem = document.createElement("li")
-    listItem.classList.add("announcementItem")
-    listItem.textContent = announcement
+    let listItem = `<li class="announcementItem">${announcement}</li>`
 
     announcementList.appendChild(listItem)
 })
