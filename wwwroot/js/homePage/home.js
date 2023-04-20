afa8107dc89ce82c3176f0e34b58e7f07d91be76
HRM0 <103372533+HRM0@users.noreply.github.com>
2023-04-20 18:50:09 -0400

    reverting changes

--- wwwroot/js/homePage/home.js
+++ wwwroot/js/homePage/home.js
@@ -3,7 +3,9 @@ let announcementList = document.querySelector(".announceList")
 let announcements = ["this", "that", "evenmore of this and that", "Now working: Bestiary, Ability, Actions, Afflictions, and Dice Roller"]
 
 announcements.map(announcement => {
-    let listItem = `<li class="announcementItem">${announcement}</li>`
+    let listItem = document.createElement("li")
+    listItem.classList = "announcementItem"
+    listItem.innerText = announcement
 
     announcementList.appendChild(listItem)
 })
